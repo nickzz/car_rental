53f3d88a861ac2a8cb1ada5c7ef25a811d160f9a
Hashif <36635662+nickzz@users.noreply.github.com>
2025-07-16 08:42:36 +0800

    add name in User

--- CarRentalAPI/Model/User.cs
+++ CarRentalAPI/Model/User.cs
@@ -1,6 +1,7 @@
 public class User
 {
     public int Id { get; set; }
+    public string? Name { get; set; }
     public string? Address { get; set; }
     public string? Email { get; set; }
     public string? PasswordHash { get; set; }
